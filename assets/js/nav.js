e7bdd41c63063e688b26b97e443a0f5206086b6e
Tom Stoecklein <tom@stoeckle.in>
2022-01-25 14:29:54 -0500

    Added turbo:render event listener for nav menu; random style tweaks

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,19 +1,21 @@
 (function() {
-  // Toggle navigation menu classes
-  const body = document.querySelector('body');
-  const hb = document.querySelector('.hamburger');
 
-  hb.onclick = function() {
-    hb.classList.toggle('hamburger--active');
-    body.classList.toggle('nav-overlay-active');
-  };
+  addEventListener("turbo:load", function(){
+    // Toggle navigation menu classes
+    const body = document.querySelector('body');
+    const hb = document.querySelector('.hamburger');
 
-  hb.addEventListener('keyup', (event) => {
-    if (event.keyCode === 27) {
-      hb.classList.remove('hamburger--active');
-      body.classList.remove('nav-overlay-active');
-      hb.removeEventListener('keyup', (event));
-    }
+    hb.onclick = function() {
+      hb.classList.toggle('hamburger--active');
+      body.classList.toggle('nav-overlay-active');
+    };
+
+    hb.addEventListener('keyup', (event) => {
+      if (event.keyCode === 27) {
+        hb.classList.remove('hamburger--active');
+        body.classList.remove('nav-overlay-active');
+        hb.removeEventListener('keyup', (event));
+      }
+    });
   });
-
 })();
